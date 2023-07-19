11fb41979ae71b2a567b950e15803c3b51ab0ae3
Alexander Somma <admin@openco.ca>
2023-07-19 09:18:03 +0000

    Flask app with contact form, static file support, and CSS styling

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import csv
+
+app = Flask(__name__, static_folder='static')
+
+# Define the route for the home page
+@app.route('/', methods=['GET'])
+def show_form():
+    return render_template('index.html')
+
+# Define the route for form submission
+@app.route('/', methods=['POST'])
+def submit_form():
+    # Retrieve the form data
+    name = request.form.get('name')
+    email = request.form.get('email')
+
+    # Save the contact information to a CSV file
+    with open('contacts.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([name, email])
+
+    return 'Thank you for submitting!'
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ templates/README.md
@@ -0,0 +1,3 @@
+# Place Jinja templates here
+
+Jinja is a modern and designer-friendly templating language for Python, modelled after Django’s templates. It is fast, widely used and secure with the optional sandboxed template execution environment.
