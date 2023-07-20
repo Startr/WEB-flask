faf8b8cb93e79d6ad98d96bd051f6a561097db99
Alexander Somma <admin@openco.ca>
2023-07-20 09:09:50 +0000

    Adding file uploads

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 import csv
+import os
+import re
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_url_path='')
 
-# Define the route for the home page
+# Create uploads directory if it doesn't exist
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
+
 @app.route('/', methods=['GET'])
 def show_form():
     return render_template('index.html')
 
-# Define the route for form submission
 @app.route('/', methods=['POST'])
 def submit_form():
-    # Retrieve the form data
     name = request.form.get('name')
     email = request.form.get('email')
+    cv = request.files.get('cv')  # Changed to use get method
 
-    # Save the contact information to a CSV file
+    # Check if name and email are provided
+    if not name or not email:
+        return "Error: Name and Email are required", 400
+
     with open('contacts.csv', 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([name, email])
+
+    if cv:
+        filename = secure_filename(cv.filename)
+        email_filename = re.sub(r'@', '_at_', email)
+        cv_filename = f"{email_filename}.pdf"
+        cv.save(os.path.join('uploads', cv_filename))
 
     return 'Thank you for submitting!'
 
